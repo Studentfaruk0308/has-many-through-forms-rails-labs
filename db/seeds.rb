43e6616b0170bea03c5ad442889a0da8cd2a859b
faruk <studentfaruk0308@gmail.com>
2022-09-12 21:05:11 +1000

    After Completion on 12092022 2100hrs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Comment.create(content: 'Very Good Post 2', user_id: 2, post_id: 2)
+Category.create(name: "Drama")
+Post.create(title: "Hello", content: "World")
+PostCategory.create(post_id: 1, category_id: 1)
+User.create(username: "Andre", email: "andre@yahoo.com")
+Comment.create(content: "Comment Content", user_id: 1, post_id: 1)
